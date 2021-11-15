fcc1fea9d2f4b22ac3c3919b99b1b1efda2556e1
Pádraig Crotty <46968070+Crotshot@users.noreply.github.com>
2021-11-15 08:59:01 +0000

    .

--- XandO/main.lua
+++ XandO/main.lua
@@ -1,114 +1,226 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+--Refactor test conditions to win, change check to win, pass params
+--move params to a lib
+
+-- Your code here
+
 local mylib = require("mylib")
 local rng = require("rng")
 local colors = require("colorsRGB")
---ai1 = require("first_space_player")
---ai2 = require("rule_based_player")
---ai3 = require("minimax_player")
 
-------------------------------------------------------------------
-local backGroup = display.newGroup();
-local mainGroup = display.newGroup();
-local uiGroup = display.newGroup();
+local ai = require("first_space_player")
+--local ai = require("random_impact_player")
+--local ai = require("minimax_player")
 
-local board = {} -- 2D representation of game board
+local backGroup = display.newGroup()
+local mainGroup = display.newGroup()
+local uiGroup = display.newGroup()
+
+local board = {} -- 2D representation of game board (logic)
 local squares = {} -- 1D representation of game board (ui, events)
 
 local players = {
- {name = "X", human = true, value = 1, wins = 0},
- {name = "O", human = true, value = 1, wins = 0}
+    {name = 'X', human = true, value = 1, wins = 0},
+    {name = 'O', human = false, value = -1, wins = 0}
 }
 
-local player = 1 --Current player
+local player = 1 -- current player
 local gameCount = 0
-local state -- 'Waiting', 'Thinking', 'Over'
+local state -- 'waiting', 'thinking', 'over'
 
 local gap = 6
 local size = (math.min(display.contentWidth, display.contentHeight) - 4 * gap) / 3
 
-local background = display.newImageRect(backGroup,"assets/images/background.png",444,794)
+local background = display.newImageRect(backGroup, "assets/images/background.png", 444, 794)
 background.x = display.contentCenterX
 background.y = display.contentCenterY
 
-local titleText, statsText, gameOverText, turnText;
-------------------------------------------------------------------
----------------------------Audio----------------------------------
-------------------------------------------------------------------
+local gameOverImage
+local titleText, statsText, turnText, gameOverText
 
 
-------------------------------------------------------------------
----------------------------Logic----------------------------------
-------------------------------------------------------------------
+local resetBoard, move
+-----------------------------------------------------------------------------------------
+-- audio
+-----------------------------------------------------------------------------------------
 
-local function isRowWin()
-    for r = 1, 3 do
-        if board[r][1] ~= 0 and board[r][1] == board[r][2] and board[r][3] == board[r][2] then
-            return r
-        end
-    end
-    return 0;
-end
 
-local function isColWin()
-    for c = 1, 3 do
-        if board[1][c] ~= 0 and board[1][c] == board[2][c] and board[3][c] == board[2][c] then
-            return c
-        end
-    end
-    return 0;
-end
 
-local function isDiagonalWin()
-    return board[1][1] ~= 0 and board[1][1] == board[2][2] and board[3][3] == board[2][2]
-end
 
-local function isAntiDiagonal()
-    return board[1][3] ~= 0 and board[1][3] == board[2][2] and board[3][1] == board[2][2]
-end
 
-local function isWin()
-    return isRowWin() > 0 or isColWin() > 0 or isDiagonalWin() or isAntiDiagonal()
-end
+-----------------------------------------------------------------------------------------
+-- UI and playing functions
+-----------------------------------------------------------------------------------------
 
-local function isTie()
-    for row = 1, 3 do
-        for col = 1, 3 do
-            if board[row][col] == 0 then return false end
-        end
-    end
-    return true;
-end
-
-------------------------------------------------------------------
---------------------UI and Player Features------------------------
-------------------------------------------------------------------
 display.setStatusBar(display.HiddenStatusBar)
 
---Function to drawline relative to center screen with given color and width
+-- function to draw line relative to center screen with given color and width
 local function drawLine(x1, y1, x2, y2, color, width)
     local line = display.newLine(backGroup,
-    display.contentCenterX + x1, display.contentCenterY + y1,
-    display.contentCenterX + x2, display.contentCenterY + y2)
+        display.contentCenterX + x1 * size, display.contentCenterY + y1 * size,
+        display.contentCenterX + x2 * size, display.contentCenterY + y2 * size
+    )
 
     color = color or "black"
+
     line:setStrokeColor(colors.RGB(color))
+    
     width = width or 8
     line.strokeWidth = width
+
 end
 
---Display message for brief interval, dimming game board
---Before reseting level
+-- display end of game message for a brief interval, dimming game board
+-- before resetting game
 local function displayMessage(message)
+    gameOverImage.alpha = 0.5
+    gameOverText.text = message
+
+    timer.performWithDelay(2500, resetBoard)
 
 end
 
+-- switch to next player by default (or to given value otherwise)
 local function nextPlayer(value)
-    player = value or (player%2) + 1
-
+    player = value or (player % 2) + 1
+    
     state = players[player].human and "waiting" or "thinking"
 
+    if state == 'thinking' then
+        local result = ai.move(board, players, player)
+        move(result)
+    end
+
 end
 
---Carries out a valid move
-local function move()
+-- carries out a valid move
+move =  function (k)
+    -- determine location of valid move
+    local square = squares[k]
 
+    -- update UIand logic representation of board
+    local filename = "assets/images/" .. players[player].name .. ".png"
+    local symbol = display.newImageRect(mainGroup, filename, size - 4 * gap, size - 4 * gap )
+    symbol.x = square.rect.x
+    symbol.y = square.rect.y
+    square.symbol = symbol
+    board[k] = players[player].value
+
+    -- check if game win
+    if mylib.isWin(board) then
+        state = "over"
+        gameCount = gameCount + 1
+        players[player].wins = players[player].wins + 1
+        displayMessage("Player " .. players[player].name .. " Wins!")
+
+    elseif mylib.isTie(board) then
+        state = "over"
+        gameCount = gameCount + 1
+        displayMessage("Game Tied")
+    else
+        nextPlayer()
+    end
+
+    -- else check if game ite
+    -- else switch to next player
 end
+
+--processes a tap evemt on the board
+-- return false if attempted move is invalid
+local function checkMove( event )
+    --determine location of tap evet on board
+    print(players[player].name .. "'s move at square ".. event.target.k)
+
+    -- current player must be human
+    if state ~= "waiting" then
+        print("\t not waiting for humna input - ignore my move")
+        return false;
+    end
+    
+    -- current square must be empty
+    if board[event.target.k] ~= 0 then
+        print("\t cannot move to non-empty space - ignore move")
+        return false;
+    end
+
+    -- implement valid move
+    move(event.target.k)
+end
+
+-- reset game state ( without unnessary destroying of UI element )
+resetBoard = function ()
+    -- tidy up UI elements
+    gameOverImage.alpha = 0;
+    gameOverText.text = ""
+    for _, square in ipairs(squares) do
+        display.remove(square.symbol)
+        square.symbol = nil
+    end
+
+    local tieCount = gameCount - players[1].wins - players[2].wins
+    local s = string.format("Games: %3d    %s: %d    %s: %d    ties: %d", gameCount, players[1].name, players[1].wins, players[2].name, players[2].wins, tieCount)
+    statsText.text = s
+
+    --reset game logic
+    board = {}
+    for k = 1, 9 do
+        board[k] = 0
+    end
+    nextPlayer(1)
+end
+
+local function createBoard()
+    -- ceneter board vetically and maxium width
+    drawLine(-0.5, - 1.5, - 0.5, 1.5)
+    drawLine(0.5, - 1.5,  0.5, 1.5)
+
+    drawLine(-1.5, -0.5, 1.5, -0.5)
+    drawLine(-1.5, 0.5, 1.5, 0.5)
+
+    squares = {}
+
+    for k = 1, 9 do
+        local row, col = mylib.k2rc(k)
+        local x = display.contentCenterX + (col - 4/2) * size
+        local y = display.contentCenterY + (row - 4/2) * size
+        local rect = display.newRect(uiGroup, x, y, size - gap, size - gap)
+        rect.alpha = 0.05
+        rect.k = k
+        rect:addEventListener("tap", checkMove)
+        squares[k] = {rect = rect}
+
+    end
+
+    titleText = display.newText(uiGroup, "X and O", 0, 0, "assets/fonts/Bangers.ttf", 30)
+    titleText.x = display.contentCenterX
+    titleText.y = display.contentCenterY - 3 * size
+    titleText:setFillColor(colors.RGB("moccasin"))
+
+    gameOverImage = display.newRect(mainGroup, 0,0, display.actualContentWidth, display.actualContentHeight)
+    gameOverImage.x = display.contentCenterX
+    gameOverImage.y = display.contentCenterY
+    gameOverImage:setFillColor(colors.RGB("black"))
+    gameOverImage.alpha = 0
+    
+    gameOverText = display.newText(uiGroup, "", 100, 200, "assets/fonts/Bangers.ttf", 50)
+    gameOverText.x = display.contentCenterX
+    gameOverText.y = display.contentCenterY - size
+    gameOverText:setFillColor(colors.RGB("pink"))
+
+    statsText = display.newText(uiGroup, "", 0, 0, "assets/fonts/Bangers.ttf", 25)
+    statsText.x = display.contentCenterX
+    statsText.y = display.contentCenterY - 2.5 * size
+    statsText:setFillColor(colors.RGB("moccasin"))
+
+    
+
+    resetBoard()
+end
+
+
+createBoard()
